15c7b951c5eb5e583da9826ee8cc3575584fd92e
MenaceHecker <thedynamic2508@gmail.com>
2025-12-25 14:11:37 -0500

    Added comments for integer pointers

--- Pointers/Pointers.py
+++ Pointers/Pointers.py
@@ -1,16 +1,18 @@
 num1 = 11
 
 num2 = num1
-
+## Displaying the values and memory addresses before and after updating num2
 print("Before num2 value is updated:")
 print("num1 =", num1)
 print("num2 =", num2)
 
+## Displaying memory addresses
 print("\nnum1 points to:", id(num1))
 print("num2 points to:", id(num2)) 
 
+## Updating num2 to a new value
 num2 = 22 
-
+ ## Displaying the values and memory addresses after updating num2. Now num1 remains unchanged while num2 points to a new memory address.
 print("\nAfter num2 value is updated:")
 print("num1 =", num1)
 print("num2 =", num2) 
