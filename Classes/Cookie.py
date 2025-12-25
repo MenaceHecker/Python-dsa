54ea033e543e105c2658789acafb606635ee8297
MenaceHecker <thedynamic2508@gmail.com>
2025-12-25 13:54:13 -0500

    Added args

--- Classes/Cookie.py
+++ Classes/Cookie.py
@@ -1,3 +1,5 @@
+
+## Class: Cookie using a constructor with arguments self and color. Then it is assigining the color argument to an instance variable called color.
 class Cookie:
     def __init__(self, color):
         self.color = color
@@ -12,10 +14,11 @@ class Cookie:
 cookie_one = Cookie('green')
 cookie_two = Cookie('blue')
 
+## Displaying the colors of both cookie instances
 print('Cookie one is', cookie_one.get_color())
 print('Cookie two is', cookie_two.get_color())
-
+##` Changing the color of cookie_one to 'yellow'
 cookie_one.set_color('yellow')
 
 print('\nCookie one is now', cookie_one.get_color())
-print('Cookie two is still', cookie_two.get_color())
+print('Cookie two is still', cookie_two.get_color()) 
