f4f0a17f0cc56b77a66bcd3bc1f70c9f7c5fd340
MenaceHecker <thedynamic2508@gmail.com>
2025-12-25 15:28:23 -0500

    Added useful comments

--- LinkedList/EXERCISE-LL-Constructor.py
+++ LinkedList/EXERCISE-LL-Constructor.py
@@ -1,8 +1,10 @@
+## Node class contains value and next pointer
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
+ ## LinkedList class with a constructor that initializes head, tail, and length       
 class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
@@ -10,9 +12,10 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
- 
+ ## Creating an instance of LinkedList with initial value 4
 my_linked_list = LinkedList(4)
 
+## Displaying the head, tail, and length of the linked list
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
