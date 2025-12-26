257d53dbdde4860c2635bd8808ff0cef27cb70d8
MenaceHecker <thedynamic2508@gmail.com>
2025-12-25 19:35:02 -0500

    Revising append method

---
+++ LinkedList/Test.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        length = self.length
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        length += 1
+        return True
+
+
+my_linked_list = LinkedList(11)
+        
