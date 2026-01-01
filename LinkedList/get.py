0dc72ace532c67e1ab803d657b35d0c1cbf80f43
MenaceHecker <thedynamic2508@gmail.com>
2026-01-01 07:25:23 +0530

    Done with remove method

--- LinkedList/get.py
+++ LinkedList/get.py
@@ -25,3 +25,17 @@ def insert(self, index, value):
         temp.next = new_node
         self.length += 1   
         return True  
+
+def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
