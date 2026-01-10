496525eed28f49d7b73a445dda17e650508900f7
MenaceHecker <thedynamic2508@gmail.com>
2026-01-10 13:22:38 +0530

    code working

--- DoublyLinkedList/pop.py
+++ DoublyLinkedList/pop.py
@@ -10,3 +10,22 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+    def pop(self):
+        if self.head is None:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp.value
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.pop()
+print(my_doubly_linked_list.head)
