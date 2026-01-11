cff64f17b93251b40fb66e72718249ae2c4c0592
MenaceHecker <thedynamic2508@gmail.com>
2026-01-11 20:18:17 +0530

    Leetcode ques completed

---
+++ DoublyLinkedList/Leetcode-Palindrome.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def is_palindrome(self):
+        if self.length <= 1:
+            return True
+        forward = self.head
+        backward = self.tail
+        for i in range(self.length // 2):
+            if forward.value != backward.value:
+                return False
+            forward = forward.next
+            backward = backward.prev
+        return True
