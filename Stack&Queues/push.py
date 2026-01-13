72151c8f81007288fc1f14b6a281dedced5c31bb
MenaceHecker <thedynamic2508@gmail.com>
2026-01-13 11:18:30 +0530

    push completed

---
+++ Stack&Queues/push.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = 1
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        return True
