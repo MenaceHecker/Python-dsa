a9d3c3099198c6c72f9fa0cb2173b89ee4acdc01
MenaceHecker <thedynamic2508@gmail.com>
2026-01-14 13:20:11 +0530

    added more functions

--- Stack&Queues/StackUsingList.py
+++ Stack&Queues/StackUsingList.py
@@ -1,3 +1,11 @@
 class Stack:
     def __init__(self):
         self.stack_list = []
+    
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if len(self.stack_list) == 0:
+            return None
+        return self.stack_list.pop()
