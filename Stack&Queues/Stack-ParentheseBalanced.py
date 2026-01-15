1beadb9f692edecac892b1644a6f769200e120e5
MenaceHecker <thedynamic2508@gmail.com>
2026-01-15 19:22:48 +0530

    ques completed

--- Stack&Queues/Stack-ParentheseBalanced.py
+++ Stack&Queues/Stack-ParentheseBalanced.py
@@ -9,4 +9,15 @@ class Stack:
         self.top = new_node
         self.height = 1
 
+## Function to check for balanced parentheses
+def is_balanced_parentheses(parentheses):
+    stack = Stack()
+    for p in parentheses:
+        if p == '(':
+            stack.push(p)
+        elif p == ')':
+            if stack.is_empty() or stack.pop() != '(':
+                return False
+    return stack.is_empty()
+
     
