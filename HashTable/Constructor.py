1bb11f67b4aa8a3502e1f8b4bc7f9fb340b2bb16
MenaceHecker <thedynamic2508@gmail.com>
2026-01-22 19:08:46 -0500

    Created hash function to make a hashmap

--- HashTable/Constructor.py
+++ HashTable/Constructor.py
@@ -1,3 +1,9 @@
 class HashTable:
     def __init__(self, size=7):
         self.data_map = [None] * size
+
+    def _hash(self, key):
+        hash_index = 0
+        for char in key:
+            hash_index = (hash_index + ord(char) * 23) % len(self.data_map)
+        return hash_index
