fd21ce34c36fad92ce025a3c10bb6fbfce81fa0a
MenaceHecker <thedynamic2508@gmail.com>
2026-01-24 14:32:11 -0500

    Added . brief description

---
+++ HashTable/Set.py
@@ -0,0 +1,9 @@
+def set_item(self, key, value):
+    """Set the value associated with key in the set.
+
+    Since this is a set, the value is ignored and only the key is stored.
+
+    Args:
+        key: The key to add to the set.
+        value: Ignored.
+    """
