88105b66ab226610372152050a8292e42b760c34
MenaceHecker <thedynamic2508@gmail.com>
2026-01-24 17:20:25 -0500

    Ques completed

---
+++ HashTable/InterviewQuestion.py
@@ -0,0 +1,12 @@
+## Given two lists find if they have a commen element
+
+def item_in_common(list1, list2):
+    my_dict = {}
+    for i in list1:
+        my_dict[i] = True
+    
+    for j in list2:
+        if j in my_dict:
+            return True
+    
+    return False
