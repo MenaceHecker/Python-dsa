bf1ba9e5f9a647b8758a640f9ddc4722e52ea827
MenaceHecker <thedynamic2508@gmail.com>
2026-02-03 13:51:44 -0500

    function completed

---
+++ Graphs/remove_vertex.py
@@ -0,0 +1,17 @@
+class Graph:
+    def __init__ (self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True 
+        return False
+    
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
