069910070f64be1c2c67107eac95ba79f30cc221
PSA - ebb-tmishra46-ebb-tmishra46 <ebb-tmishra46@gatech.edu>
2026-02-03 12:40:18 -0500

    Add edge function completed

---
+++ Graphs/add_edge.py
@@ -0,0 +1,18 @@
+class Graph:
+    def __init__ (self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True 
+        return False
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.key() and v2 in self.adj_list.key():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
+
