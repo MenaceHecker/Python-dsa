d352daf819468dbd8998e194d802db04336cd21d
MenaceHecker <thedynamic2508@gmail.com>
2026-02-09 18:01:08 -0500

    Added helper functions

--- Heaps/Remove.py
+++ Heaps/Remove.py
@@ -1,13 +1,29 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]   
+    
     def remove(self):
-    if len(self.head) == 0:
-        return None
+        if len(self.heap) == 0:
+            return None
 
-    if len(self.head) == 1:
-        return self.head.pop()
+        if len(self.heap) == 1:
+            return self.heap.pop()
 
-    max_value = self.head[0]
-    self.head[0] = self.head.pop()
-    self.sink_down(0)
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
 
-    return max_value
+        return max_value
      
