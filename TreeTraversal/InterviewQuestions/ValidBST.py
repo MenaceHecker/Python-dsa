bbf1d8092fde3158b2c7d1db14afa0da02836f07
MenaceHecker <thedynamic2508@gmail.com>
2026-03-04 20:50:26 -0500

    Function is_valid_bst checks if result from in order dfs is sorted

--- TreeTraversal/InterviewQuestions/ValidBST.py
+++ TreeTraversal/InterviewQuestions/ValidBST.py
@@ -1,3 +1,69 @@
 ## You are tasked with writing a method called is_valid_bst in the BinarySearchTree class 
 ## that checks whether a binary search tree is a valid binary search tree.
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while (True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else: 
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value) 
+            if current_node.right is not None:
+                traverse(current_node.right)          
+        traverse(self.root)
+        return results
+        
+    def is_valid_bst(self):
+        results = self.dfs_in_order()
+        i = 1
+        while i < len(results):
+            if results[i] < results[i-1]:
+                return False
+            i += 1
+        return True        
+            
+            
+
+
+
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print("BST is valid:")
+print(my_tree.is_valid_bst())
