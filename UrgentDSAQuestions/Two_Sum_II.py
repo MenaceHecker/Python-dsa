1e9fae69d86cb0d659f040970bf1c0d58ddef391
MenaceHecker <thedynamic2508@gmail.com>
2026-08-29 19:56:47 -0400

    Solution completed

--- UrgentDSAQuestions/Two_Sum_II.py
+++ UrgentDSAQuestions/Two_Sum_II.py
@@ -2,3 +2,21 @@
 ## find two numbers such that they add up to a specific target number.
 
 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        nums = {}
+        dif = 0
+        l = len(numbers)
+        i = 0
+        result = []
+        while i < l:
+            dif = target - numbers[i]
+            if dif in nums:
+                result.append(nums[dif]+1)
+                result.append(i+1)
+                return result
+            nums[numbers[i]] = i    
+            i += 1
+
+
+        
