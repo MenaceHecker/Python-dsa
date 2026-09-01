9fad541311d054356d51627d95d40e77a0b9ee5c
MenaceHecker <thedynamic2508@gmail.com>
2026-09-01 11:45:30 -0400

    Code completed

--- UrgentDSAQuestions/RottingOranges.py
+++ UrgentDSAQuestions/RottingOranges.py
@@ -7,3 +7,44 @@
 ## Return the minimum number of minutes that must elapse until no cell has a fresh orange
 ## If this is impossible, return -1.
 
+from collections import deque
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        queue = deque()
+        fresh = 0 
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r,c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+        directions = [
+                (-1,0),
+                (1,0),
+                (0,-1),
+                (0,1) 
+            ]
+        minutes = 0 
+        while queue and fresh > 0:
+            for _ in range(len(queue)):
+                r,c = queue.popleft()
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
+                    if ( 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1):
+                        grid[nr][nc] = 2
+                        queue.append((nr,nc))
+                        fresh -= 1
+                
+            minutes += 1
+        if fresh > 0:
+            return -1
+        return minutes
+
+                    
+
+        
