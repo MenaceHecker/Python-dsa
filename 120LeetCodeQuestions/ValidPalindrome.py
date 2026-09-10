945871816d1f24684df0bc10e4414dc2306a4b04
MenaceHecker <thedynamic2508@gmail.com>
2026-09-10 13:03:51 -0400

    Code completed

--- 120LeetCodeQuestions/ValidPalindrome.py
+++ 120LeetCodeQuestions/ValidPalindrome.py
@@ -2,3 +2,16 @@
 # removing all non-alphanumeric characters, it reads the same forward and backward. 
 # Alphanumeric characters include letters and numbers.
 ##Given a string s, return true if it is a palindrome, or false otherwise.
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1 = ""
+        for i in s:
+            if i.isnumeric() == True or i.isalpha() == True:
+                s1 += i
+        s1 = s1.lower()
+        s2 = s1[::-1]
+        if s1 == s2:
+            return True
+        return False
+        
