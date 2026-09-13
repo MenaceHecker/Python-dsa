6f3dd8b2b8a6d55f574a80450614d5db31a89773
MenaceHecker <thedynamic2508@gmail.com>
2026-09-13 16:16:54 -0400

    Completed the problem

--- 120LeetCodeQuestions/4.ContainerWithMostWater.py
+++ 120LeetCodeQuestions/4.ContainerWithMostWater.py
@@ -4,3 +4,19 @@
 ## Find two lines that together with the x-axis form a container, such that the container contains the most water.
 ## Return the maximum amount of water a container can store.
 ## Notice that you may not slant the container.
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i = 0
+        j = len(height) - 1
+        area_max = 0
+        area = 1
+        while i < j:
+            second = min(height[i],height[j])
+            area = (j - i) * second
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -=1 
+            area_max = max(area, area_max)
+        return area_max
