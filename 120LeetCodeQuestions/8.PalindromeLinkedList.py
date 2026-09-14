552e7e5f08da4dbe54c12e2d86df3bd119e6b59d
MenaceHecker <thedynamic2508@gmail.com>
2026-09-13 20:22:01 -0400

    Implemented the solution with List approach

--- 120LeetCodeQuestions/8.PalindromeLinkedList.py
+++ 120LeetCodeQuestions/8.PalindromeLinkedList.py
@@ -1,3 +1,16 @@
 ## Given the head of a singly linked list, 
 ## return true if it is a palindrome or false otherwise.
 
+## Implemented with list approach
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        temp = head
+        arr = []
+        while temp is not None:
+            arr.append(temp.val)
+            temp = temp.next
+        return arr == arr[::-1]
