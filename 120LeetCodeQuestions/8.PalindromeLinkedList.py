a371cf3fc539910a9f4bb78657778a52870436e6
MenaceHecker <thedynamic2508@gmail.com>
2026-09-13 20:26:23 -0400

    Implemented the solution with Stack approach

--- 120LeetCodeQuestions/8.PalindromeLinkedList.py
+++ 120LeetCodeQuestions/8.PalindromeLinkedList.py
@@ -14,3 +14,30 @@ class Solution:
             arr.append(temp.val)
             temp = temp.next
         return arr == arr[::-1]
+
+
+## Implementation with stack approach
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+
+        # Store all node values in the stack
+        curr = head
+        while curr:
+            stack.append(curr.val)
+            curr = curr.next
+
+        # Compare linked-list values with reversed stack values
+        curr = head
+        while curr:
+            if curr.val != stack.pop():
+                return False
+
+            curr = curr.next
+
+        return True
