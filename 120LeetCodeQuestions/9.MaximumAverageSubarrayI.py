b3a7b736b11493befe704e75f326fb92ef69278f
MenaceHecker <thedynamic2508@gmail.com>
2026-09-15 13:22:30 -0400

    Implemented the solution using fix sliding window

--- 120LeetCodeQuestions/9.MaximumAverageSubarrayI.py
+++ 120LeetCodeQuestions/9.MaximumAverageSubarrayI.py
@@ -2,4 +2,13 @@
 ## Find a contiguous subarray whose length is equal to k 
 ## that has the maximum average value and return this value. 
 
-
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+        for i in range(k,len(nums)):
+            window_sum += nums[i]
+            window_sum -= nums[i-k]
+            max_sum = max(max_sum, window_sum)
+        avg = max_sum / k
+        return avg
