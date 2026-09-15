f1bf7e7ceed572eb01e69e2034ce94d628bc75d2
MenaceHecker <thedynamic2508@gmail.com>
2026-09-15 13:21:50 -0400

    Added the question

---
+++ 120LeetCodeQuestions/9.MaximumAverageSubarrayI.py
@@ -0,0 +1,5 @@
+## You are given an integer array nums consisting of n elements, and an integer k.
+## Find a contiguous subarray whose length is equal to k 
+## that has the maximum average value and return this value. 
+
+
