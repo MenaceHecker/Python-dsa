71e745d07a7099a3001766bc892ca0a2b352f869
MenaceHecker <thedynamic2508@gmail.com>
2026-09-16 09:16:56 -0400

    Implemented the solution using sliding window

--- 120LeetCodeQuestions/11.LongestSubstringWithoutRepeatingCharacter.py
+++ 120LeetCodeQuestions/11.LongestSubstringWithoutRepeatingCharacter.py
@@ -2,3 +2,16 @@
 ## substring without duplicate characters.
 
 ## Main solution uses sliding window technique with two pointers
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        check = set()
+        l = 0
+        lon = 0
+        for r in range(len(s)):
+            while s[r] in check:
+                check.remove(s[l])
+                l += 1
+            check.add(s[r])
+            lon = max(lon, r-l+1)
+        return lon
